dd53ea467fe9dc38e012466957f1e0201fbef3a6
leeway64 <lee.way56@comcast.net>
2022-12-04 16:37:21 -0800

    Update get-currency-exchange-rates script to get CSV through curl cmd
    - Update gitignore to ignore CSV files

--- lib/get-currency-exchange-rates.sh
+++ lib/get-currency-exchange-rates.sh
@@ -5,24 +5,28 @@
 main()
 {
     # Download the exchange rates CSV file into the include directory
-	sudo wget https://www.federalreserve.gov/datadownload/Output.aspx?rel=H10&series=c5d6e0edf324b2fb28d73bcacafaaa02&lastobs=120&from=&to=&filetype=csv&label=include&layout=seriescolumn&type=package -P ./include/
+	curl -o ./include/FRB_H10.csv "https://www.federalreserve.gov/datadownload/Output.aspx?rel=H10&series=c5d6e0edf324b2fb28d73bcacafaaa02&lastobs=120&from=&to=&filetype=csv&label=include&layout=seriescolumn&type=package"
 
 	# Zip up the CSV file
-	zip exchange-rates.zip ./include/*
+	zip ./include/exchange-rates.zip ./include/FRB_H10.csv
+    rm ./include/FRB_H10.csv
 
 	# Unzip the CSV file
 	unzip ./include/exchange-rates.zip
+    rm -r ./include/exchange-rates.zip
 
     echo "Data successfully zipped and unzipped" | cat
 
 	# Compress the CSV into a tarball with gzip
-	tar -cvzf exchange-rates.tar.gz ./include/*
+	tar -cvzf ./include/exchange-rates.tar.gz ./include/FRB_H10.csv
+    rm ./include/FRB_H10.csv
 
 	# Extract the tarball's contents
 	tar -xvf ./include/exchange-rates.tar.gz
+	rm -r ./include/exchange-rates.tar.gz
 
     echo "Data successfully compressed and decompressed through gzip"
-	cat data-download-success.message.txt | head | tail
+	cat ./lib/data-download-success.message.txt | head | tail
 }
 
 
